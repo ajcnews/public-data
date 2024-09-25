970344a1006389f2ea1e2543103c757b41ed8906
Rahul Deshpande <rahul.deshpande@ajc.com>
2024-09-25 17:33:02 -0400

    adds timezone information to datetime objects

--- power-outages/convert.py
+++ power-outages/convert.py
@@ -2,6 +2,7 @@ import requests
 from datetime import datetime
 import csv
 import shutil
+from zoneinfo import ZoneInfo
 
 url = "https://services1.arcgis.com/2iUE8l8JKrP2tygQ/ArcGIS/rest/services/Georgia_Power_Outages_View/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&f=json"
 response = requests.get(url)
@@ -12,12 +13,15 @@ features = data["features"]
 csv_file = f'data/outages.csv'
 csv_columns = ["objectid_1", "name", "state_name", "outage_count", "county_status", "customer_count", "percent_out", "county_id", "timestamp", "global_id", "edit_date"]
 
+utc_zone = ZoneInfo("UTC")
+ny_zone = ZoneInfo("America/New_York")
+
 with open(csv_file, mode='w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=csv_columns)
     writer.writeheader()
     for feature in features:
         attributes = feature["attributes"]
-        attributes["timestamp"] = datetime.fromtimestamp(attributes.pop("TimeStamp") / 1000.0).strftime("%Y-%m-%d %H:%M:%S")
+
         attributes["objectid_1"] = attributes.pop("OBJECTID_1")
         attributes["name"] = attributes.pop("NAME")
         attributes["state_name"] = attributes.pop("STATE_NAME")
@@ -27,7 +31,11 @@ with open(csv_file, mode='w', newline='') as file:
         attributes["percent_out"] = attributes.pop("PercentOut")
         attributes["county_id"] = attributes.pop("CountyId")
         attributes["global_id"] = attributes.pop("GlobalID")
-        attributes["edit_date"] = datetime.fromtimestamp(attributes.pop("EditDate") / 1000.0).strftime("%Y-%m-%d %H:%M:%S")
+
+        attributes["timestamp"] = datetime.fromtimestamp(attributes.pop("TimeStamp") / 1000.0).replace(tzinfo=ny_zone).strftime("%Y-%m-%d %H:%M:%S")
+
+        attributes["edit_date"] = datetime.fromtimestamp(attributes.pop("EditDate") / 1000.0).replace(tzinfo=ny_zone).strftime("%Y-%m-%d %H:%M:%S")
+
         writer.writerow({key: attributes[key] for key in csv_columns})
 
 shutil.copy(csv_file, f"data/outages_{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv")
